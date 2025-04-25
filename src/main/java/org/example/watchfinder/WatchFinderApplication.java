7e28309e0c2005c73b38648cb2fde2c2ad972911
MaCorMa <manuel.correcher@gmail.com>
2025-04-25 16:46:53 +0200

    .env para ocultar variables entorno

--- src/main/java/org/example/watchfinder/WatchFinderApplication.java
+++ src/main/java/org/example/watchfinder/WatchFinderApplication.java
@@ -1,11 +1,14 @@
 package org.example.watchfinder;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class WatchFinderApplication {
 
-    public static void main(String[] args) { SpringApplication.run(WatchFinderApplication.class, args); }
-
+    public static void main(String[] args) { SpringApplication.run(WatchFinderApplication.class, args);}
 }
